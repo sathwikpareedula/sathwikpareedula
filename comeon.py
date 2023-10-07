3bc41bb4e9bdfe35799113e7d7488a7f108ae7d1
sathwikpareedula <147267090+sathwikpareedula@users.noreply.github.com>
2023-10-07 16:47:11 -0500

    Update comeon.py

--- comeon.py
+++ comeon.py
@@ -21,3 +21,25 @@ print("Image captured and saved as 'captured_image.jpg'")
 cv2.imshow('Captured Image', frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+import cv2
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)  # 0 represents the default camera, change if needed
+
+# Capture a frame
+ret, frame = cap.read()
+
+# Release the camera
+cap.release()
+
+# Save the frame as an image
+cv2.imwrite('captured_image.jpg', frame)
+
+# Display a message
+print("Image captured and saved as 'captured_image.jpg'")
+
+# Optional: Display the captured image
+cv2.imshow('Captured Image', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
