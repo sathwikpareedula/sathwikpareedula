6155115c8d8460db1d8bed26db2baaaf9a560eec
sathwikpareedula <147267090+sathwikpareedula@users.noreply.github.com>
2023-10-07 17:03:43 -0500

    Update comeon.py

--- comeon.py
+++ comeon.py
@@ -3,22 +3,22 @@ from io import BytesIO
 import requests
 
 def capture_image():
-    # Assuming you have a camera accessible via a URL
-    # Replace 'CAMERA_URL' with the actual URL of your camera
-    camera_url = 'CAMERA_URL'
+    camera_url = 'YOUR_CAMERA_URL'  # Replace with the actual camera URL
 
-    # Capture the image from the camera
-    response = requests.get(camera_url)
+    try:
+        response = requests.get(camera_url)
+        response.raise_for_status()  # Raise an error for bad responses (e.g., 404, 500)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching image: {e}")
+        return None
+
     image_data = BytesIO(response.content)
-
-    # Open the image using Pillow
     image = Image.open(image_data)
 
     return image
 
 if __name__ == "__main__":
-    # Capture an image
     captured_image = capture_image()
 
-    # Display the image
-    captured_image.show()
+    if captured_image:
+        captured_image.show()
