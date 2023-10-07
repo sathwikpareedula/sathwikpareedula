379d32d0617ceb1b5a2cc6c69fabf755f2a7fc6f
sathwikpareedula <147267090+sathwikpareedula@users.noreply.github.com>
2023-10-07 16:23:15 -0500

    Update comeon.py

--- comeon.py
+++ comeon.py
@@ -1,1 +1,20 @@
+import cv2
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)  # 0 represents the default camera, if you have multiple, it might be 1, 2, etc.
+
+while True:
+    # Capture a frame
+    ret, frame = cap.read()
+
+    # Display the frame
+    cv2.imshow('Camera Feed', frame)
+
+    # Check for user input to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
 
