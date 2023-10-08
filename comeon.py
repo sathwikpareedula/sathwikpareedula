2632de427cd5a2be87d924d2b8bd37f859430c88
sathwikpareedula <147267090+sathwikpareedula@users.noreply.github.com>
2023-10-07 19:33:57 -0500

    Update comeon.py

--- comeon.py
+++ comeon.py
@@ -1,24 +1,113 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from PIL import Image
-from io import BytesIO
-import requests
+import os
 
-def capture_image():
-    # Assuming you have a camera accessible via a URL
-    # Replace 'CAMERA_URL' with the actual URL of your camera
-    camera_url = 'CAMERA_URL'
+MODELSPATH = 'waste_model.h5'
+DATAPATH = 'waste1.jpg'
+IMAGE_DIR = 'images'  # Directory containing recycling images
 
-    # Capture the image from the camera
-    response = requests.get(camera_url)
-    image_data = BytesIO(response.content)
+# Function to load data
+@st.cache
+def load_data():
+    img = Image.open(DATAPATH)
+    return img
 
-    # Open the image using Pillow
-    image = Image.open(image_data)
+# Function to load models
+def load_models():
+    model = load_model(MODELSPATH, compile=False)
+    return model
 
-    return image
+# Set page title and background
+st.set_page_config(
+    page_title="Recycling Importance & Waste Prediction",
+    page_icon="🔄",
+    layout="wide",
+)
 
-if __name__ == "__main__":
-    # Capture an image
-    captured_image = capture_image()
+# Add a title with a background image
+st.markdown(
+    """
+    <style>
+        .title-text {
+            font-size: 36px;
+            font-weight: bold;
+            color: #ffffff;
+        }
+        .title-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-image: url('https://img.freepik.com/free-vector/green-set-recycled-signs_78370-662.jpg?w=2000');
+            background-size: cover;
+            background-position: center;
+            height: 300px;
+        }
+    </style>
+    <div class="title-container">
+        <div class="title-text">The Importance of Recycling</div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-    # Display the image
-    captured_image.show()
+# Add some information about recycling
+st.markdown(
+    """
+    Recycling is crucial for preserving our environment and conserving natural resources. It helps reduce pollution, 
+    saves energy, and reduces the need for raw materials. By recycling, we can contribute to a more sustainable and 
+    eco-friendly future.
+    """
+)
+
+# Add images related to recycling
+recycling_images = ['https://img.freepik.com/free-vector/green-set-recycled-signs_78370-662.jpg?w=2000']  
+st.image(recycling_images, caption=["Recycling Image 1"], width=300)
+
+# Sidebar for page selection
+st.sidebar.title('Select options:')
+page = st.sidebar.selectbox("Choose a page:", ["Sample Data", "Upload an Image"])
+
+if page == "Sample Data":
+    st.header("Sample prediction for waste")
+    if st.checkbox('Show Sample Data'):
+        st.info("Sample image:")
+        image = load_data()
+        st.image(image, caption='Sample Data', use_column_width=True)
+        image = image.resize((64, 64))
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = image.reshape((1, 64, 64, 3))
+        st.subheader("Check waste prediction")
+        if st.checkbox('Show Prediction of simple image'):
+            model = load_models()
+            result = model.predict(image)
+            st.write(result)
+            if result[0][0] == 1:
+                prediction = 'Recyclable Waste'
+            else:
+                prediction = 'Organic Waste'
+            st.write(prediction)
+            st.success("It is prediction for sample image!")
+
+if page == "Upload an Image":
+    st.header("Your prediction of waste")
+    uploaded_file = st.file_uploader("Choose your image", type=["jpg", "png"])
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+        st.info("Show your image:")
+        st.image(img, caption="Upload image", use_column_width=True)
+        img = img.resize((64, 64))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = img.reshape((1, 64, 64, 3))
+        st.subheader("Check waste prediction")
+        if st.checkbox('Show Prediction of your image'):
+            model = load_models()
+            result = model.predict(img)
+            st.write(result)
+            if result[0][0] == 1:
+                prediction = 'Recyclable Waste'
+            else:
+                prediction = 'Organic Waste'
+            st.write(prediction)
+            st.success("That is your prediction!")
